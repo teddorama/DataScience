3861c679a439c76b1652685a12b3cd5b5c2e07d8
teddorama <30339656+teddorama@users.noreply.github.com>
2017-08-07 17:14:12 +0900

    zip for 100MB+

---
+++ NeuralNetwork/tedd_sci.py
@@ -0,0 +1,67 @@
+import numpy as np
+import scipy.special
+from matplotlib import pyplot as plt
+
+
+class NeuralNetwork:
+    """ Neural Network
+    """
+
+    def __init__(self, inputnodes, hiddennodes, outputnodes, learningrate):
+        self.inodes = inputnodes
+        self.hnodes = hiddennodes
+        self.onodes = outputnodes
+        self.lr = learningrate
+
+        self.wih = np.random.normal(0.0, pow(self.hnodes, -0.5),
+                                    (self.hnodes, self.inodes))
+        self.who = np.random.normal(0.0, pow(self.onodes, -0.5),
+                                    (self.onodes, self.hnodes))
+
+        self.inputs = None
+        self.targets = None
+        self.hidden_inputs = None
+        self.hidden_outputs = None
+        self.final_inputs = None
+        self.final_outputs = None
+
+        self.activation_function = lambda x: scipy.special.expit(x)
+        pass
+
+    def train(self, inputs_list, targets_list):
+        self.targets = np.array(targets_list, ndmin=2).T
+        self.query(inputs_list)
+
+        output_errors = self.targets - self.final_outputs
+        hidden_errors = np.dot(self.who.T, output_errors)
+
+        self.who += self.lr * np.dot(output_errors * self.final_outputs * (1.0-self.final_outputs), np.transpose(self.hidden_outputs))
+        self.wih += self.lr * np.dot(hidden_errors * self.hidden_outputs * (1.0-self.hidden_outputs), np.transpose(self.inputs))
+
+        pass
+
+    def query(self, inputs_list):
+        self.inputs = np.array(inputs_list, ndmin=2).T
+
+        self.hidden_inputs = np.dot(self.wih, self.inputs)
+        self.hidden_outputs = self.activation_function(self.hidden_inputs)
+
+        self.final_inputs = np.dot(self.who, self.hidden_outputs)
+        self.final_outputs = self.activation_function(self.final_inputs)
+
+        return self.final_outputs
+
+
+if __name__ == "__main__":
+    n = NeuralNetwork(3, 3, 3, 0.3)
+    print(n.wih)
+    print(n.query([1.0, 0.5, -1.5]))
+
+    #data_file = open("DataScience/NeuralNetwork/mnist_dataset/mnist_train_100.csv", "r")
+    data_file = open("mnist_dataset/mnist_train_100.csv", "r")
+    data_list = data_file.readlines()
+    data_file.close()
+
+    all_values = data_list[0].split(',')
+    image_array = np.asfarray(all_values[1:]).reshape((28, 28))
+    plt.imshow(image_array, cmap='Greys', interpolation='None')
---
+++ NeuralNetwork/test_tedd_sci.py
@@ -0,0 +1,62 @@
+import zipfile
+import numpy as np
+
+import tedd_sci
+
+
+n = tedd_sci.NeuralNetwork(784, 100, 10, 0.3)
+training_data_file = ''
+with zipfile.ZipFile('mnist_dataset/mnist_train.zip', 'r') as z:
+    training_data_file = z.open('mnist_train.csv', "r")
+
+
+
+#training_data_file = open("DataScience/NeuralNetwork/mnist_dataset/mnist_train_100.csv", "r")
+#training_data_file = open("mnist_dataset/mnist_train_100.csv", "r")
+#training_data_file = open("mnist_dataset/mnist_train.csv", "r")
+training_data_list = training_data_file.readlines()
+training_data_file.close()
+
+epochs = 5
+for e in range(epochs):
+    for record in training_data_list:
+        record = record.decode("UTF-8")
+
+        all_values = record.split(',')
+        inputs = (np.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
+
+        targets = np.zeros(10) + 0.01
+
+        targets[int(all_values[0])] = 0.99
+        n.train(inputs, targets)
+
+        pass
+    pass
+
+#test_data_file = open("DataScience/NeuralNetwork/mnist_dataset/mnist_test_10.csv", "r")
+#test_data_file = open("mnist_dataset/mnist_test_10.csv", "r")
+test_data_file = open("mnist_dataset/mnist_test.csv", "r")
+test_data_list = test_data_file.readlines()
+test_data_file.close()
+
+scorecard = []
+
+for record in test_data_list:
+    all_values = record.split(',')
+    correct_label = int(all_values[0])
+    print("Value = ", correct_label)
+
+    inputs = (np.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
+    outputs = n.query(inputs)
+    label = np.argmax(outputs)
+    print("Guess = ", label)
+
+    if (label == correct_label):
+        scorecard.append(1)
+    else:
+        scorecard.append(0)
+        pass
+    pass
+
+scorecard_array = np.asarray(scorecard)
+print("performance =", scorecard_array.sum()/scorecard_array.size)
